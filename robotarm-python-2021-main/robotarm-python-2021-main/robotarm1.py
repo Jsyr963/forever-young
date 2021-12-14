b05c2da7e0d1b0e910dc498dc08a3637909bad6a
Jsyr963 <89968301+Jsyr963@users.noreply.github.com>
2021-12-14 10:59:56 +0100

    ?de commit!

--- robotarm-python-2021-main/robotarm-python-2021-main/robotarm1.py
+++ robotarm-python-2021-main/robotarm-python-2021-main/robotarm1.py
@@ -6,12 +6,11 @@ import random
 robotArm = RobotArm('exercise 1')
 
 # Jouw python instructies zet je vanaf hier:
-for i in range(1):
-    robotArm.moveRight()
-    robotArm.grab()
-for i in range(1):
-    robotArm.moveLeft()
-    robotArm.drop()
+
+robotArm.moveRight()
+robotArm.grab()
+robotArm.moveLeft()
+robotArm.drop()
 
     # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait(3)
